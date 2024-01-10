aaaa7f990bda1de6eaeadf3540fbc6b213438f34
Pavel Davydov <Davydov.works@gmail.com>
2024-01-10 11:59:34 -0500

    add MiddleTask 2versions

---
+++ src/main/java/HomeTask/MinMaxMid.java
@@ -0,0 +1,35 @@
+package HomeTask;
+
+import java.util.Scanner;
+
+public class MinMaxMid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter any 3 numbers : ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int min, max, mid;
+        if (num1 <= num2 && num1 <= num3) {
+            min = num1;
+        } else if (num2 <= num1 && num2 <= num3) {
+            min = num2;
+        } else {
+            min = num3;
+        }
+        if (num1 >= num2 && num1 >= num3) {
+            max = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            max = num2;
+        } else {
+            max = num3;
+        }
+    mid = (num1 + num2 + num3) /3;
+        System.out.println("Smallest: " + min);
+        System.out.println("Largest: " + max);
+        System.out.println("Average: " + mid);
+    }
+
+
+
+}
---
+++ src/main/java/HomeTask/MinMaxMidwith/MethodMath.java
@@ -0,0 +1,22 @@
+package HomeTask.MinMaxMidwith;
+
+import java.util.Scanner;
+
+public class MethodMath {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter any 3 numbers : ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        int min = Math.min(Math.min(num1, num2), num3);
+        int max = Math.max(Math.max(num1, num2), num3);
+        int mid = (num1 + num2 + num3) / 3;
+
+        System.out.println("Min : " + min);
+        System.out.println("Max : " + max);
+        System.out.println("Mid : " + mid);
+    }
+
+}
