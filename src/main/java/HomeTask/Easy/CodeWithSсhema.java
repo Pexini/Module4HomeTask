709cdd65a560521d6c2da1ccd3d82c3aa638fb76
Pavel Davydov <Davydov.works@gmail.com>
2024-01-11 14:50:57 -0500

    Modified Easy Task2 Schema

--- src/main/java/HomeTask/Easy/CodeWithSсhema.java
+++ src/main/java/HomeTask/Easy/CodeWithSсhema.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 
 public class CodeWithSсhema {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter value X :");
-        int x = scanner.nextInt();
+        int x = -2;
         int y;
         if (x > 0) {
             y = 2 * x;
         } else {
             y = -2 * x;
         }
-        System.out.println(" Your result ; " + y);
+        System.out.println(" Your result : " + y);
     }
 }
