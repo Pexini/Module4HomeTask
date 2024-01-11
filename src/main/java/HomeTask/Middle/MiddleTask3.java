eb3ce0364f95e3495097c04e03aa564c645e2d99
Pavel Davydov <Davydov.works@gmail.com>
2024-01-11 04:36:14 -0500

    add Middle task3

--- src/main/java/HomeTask/Middle/MiddleTask3.java
+++ src/main/java/HomeTask/Middle/MiddleTask3.java
@@ -1,4 +1,19 @@
 package HomeTask.Middle;
 
+import java.util.Scanner;
+
 public class MiddleTask3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter 3 side of triangle : ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a > b + c || b > a + c || c > a + b) {
+            System.out.println( "Sorry triangle is not real");
+        }else {
+            System.out.println("Yes, triangle is real");
+        }
+    }
 }
